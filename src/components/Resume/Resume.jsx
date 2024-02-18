fabfd62ac3cb6f885986de039f9c5de399ff3a51
Wolffkran <liebherr2008@live.com>
2024-02-18 13:55:39 -0500

    Additional styling

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,5 +1,3 @@
-// Resume.jsx
-
 import React from 'react';
 
 const Resume = () => {
@@ -8,15 +6,16 @@ const Resume = () => {
       <h2>Resume</h2>
       <p>Download my resume: <a href={process.env.PUBLIC_URL + '/components/Resume/Raymond_Lacrouts_Resume.PDF'} download="Raymond_Lacrouts_Resume.PDF" target="_blank" rel="noopener noreferrer">Resume.pdf</a></p>
 
-
-      <h3>Skills</h3>
-      <ul>
-        <li>Web Development</li>
-        <li>React.js</li>
-        <li>JavaScript (ES6+)</li>
-        <li>HTML5, CSS3</li>
-        {/* Add more skills as needed */}
-      </ul>
+      <section>
+        <h3>Skills</h3>
+        <ul>
+          <li>Web Development</li>
+          <li>React.js</li>
+          <li>JavaScript (ES6+)</li>
+          <li>HTML5, CSS3</li>
+          {/* Add more skills as needed */}
+        </ul>
+      </section>
 
       {/* Add more sections like Education, Experience, etc. */}
     </div>
