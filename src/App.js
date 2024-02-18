adbdd0958c1f4cdc91b12a58ce9fe20077bff5ca
Wolffkran <liebherr2008@live.com>
2024-02-18 09:41:45 -0500

    js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/header/Header';
+import AboutMe from './components/AboutMe/AboutMe';
+import Portfolio from './components/Portfolio/Portfolio';
+import ContactMe from './components/Contact/ContactForm'; // or ContactMe component
+import Resume from './components/Resume/Resume';
+import Footer from './components/Footer/Footer';
+import './styles/App-tailwind.css';
+import './styles/Header-tailwind.css';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Portfolio />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<ContactMe />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
